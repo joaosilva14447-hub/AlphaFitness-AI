998c44007c0f87a47a91f73de1381ce364eb050a
joaosilva14447-hub <joaosilva14447@gmail.com>
2026-03-07 08:27:32 +0000

    Create 01_Workout_Architect.py

---
+++ pages/01_Workout_Architect.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Workout Architect", layout="wide")
+st.title("✦ Workout Architect ✦")
+st.write("Esta página irá gerar os teus treinos de elite.")
+
+# Sidebar de teste
+with st.sidebar:
+    st.success("Módulo de Treino Ativo")
